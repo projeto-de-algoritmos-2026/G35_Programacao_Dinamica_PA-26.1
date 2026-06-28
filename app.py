7c4f3403caa88cd75ae8f5b4d90568eebc0b96e4
Henrique Alencar <henriquemalencar@yahoo.com.br>
2026-06-28 16:45:28 -0300

    feat: html inicial

--- app.py
+++ app.py
@@ -3,10 +3,47 @@ from mochila import otimizar_estudos
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
 @app.route('/gerar_plano', methods=['POST'])
 def gerar_plano():
+    try:
 
-    horas_disponiveis = float(request.form.get('tempo_livre'))
-    plano, ganho_total = otimizar_estudos(disciplinas, horas_disponiveis)
-    
-    return render_template('resultado.html', plano=plano, ganho=ganho_total)
+        tempo_livre = float(request.form.get('tempo_livre', 0))
+        materias = request.form.getlist('materia[]')
+        horas = request.form.getlist('horas[]')
+        dificuldades = request.form.getlist('dif[]')
+        dias_prova = request.form.getlist('dias[]')
+        
+        disciplinas = []
+
+        for i in range(len(materias)):
+            nome = materias[i]
+
+            if nome and nome.strip() != "":
+                disciplinas.append({
+                    "nome": nome,
+                    "horas_necessarias": float(horas[i]),
+                    "dificuldade": int(dificuldades[i]),
+                    "dias_prova": int(dias_prova[i])
+                })
+
+        plano, ganho_total = otimizar_estudos(disciplinas, tempo_livre)
+
+        tempo_gasto = sum(d['horas_necessarias'] for d in plano)
+
+        return render_template(
+            'index.html',
+            plano=plano,
+            ganho=ganho_total,
+            tempo_gasto=tempo_gasto,
+            tempo_livre_original=tempo_livre
+        )
+        
+    except ValueError:
+        return "Erro ao processar os dados. Certifique-se de usar números válidos.", 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- mochila.py
+++ mochila.py
@@ -1,9 +1,8 @@
-def calcular_importancia(peso_disciplina, dificuldade, dias_prova):
+def calcular_importancia(dificuldade, dias_prova):
 
     dias_reais = max(1, dias_prova)
-
-    importancia = (peso_disciplina * dificuldade * 100) / dias_reais
-
+    importancia = (dificuldade * 100) / dias_reais
+    
     return round(importancia, 2)
 
 def otimizar_estudos(disciplinas, horas_disponiveis):
@@ -13,39 +12,32 @@ def otimizar_estudos(disciplinas, horas_disponiveis):
     
     valores = []
     pesos_blocos = []
-
+    
     for d in disciplinas:
-        valores.append(calcular_importancia(d['peso'], d['dificuldade'], d['dias_prova']))
-
+        valores.append(calcular_importancia(d['dificuldade'], d['dias_prova']))
         pesos_blocos.append(int(d['horas_necessarias'] * 2))
 
     dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
-
+    
     for i in range(1, n + 1):
         for w in range(1, W + 1):
             peso_atual = pesos_blocos[i - 1]
             valor_atual = valores[i - 1]
             
             if peso_atual <= w:
-
                 dp[i][w] = max(valor_atual + dp[i - 1][w - peso_atual], dp[i - 1][w])
             else:
-
                 dp[i][w] = dp[i - 1][w]
-
+                
     itens_selecionados = []
     w_atual = W
-
-    for i in range(n, 0, -1):
-
-        if dp[i][w_atual] != dp[i - 1][w_atual]:
-
-            itens_selecionados.append(disciplinas[i - 1])
-
-            w_atual -= pesos_blocos[i - 1]
-
-    itens_selecionados.reverse()
     
+    for i in range(n, 0, -1):
+        if dp[i][w_atual] != dp[i - 1][w_atual]:
+            itens_selecionados.append(disciplinas[i - 1])
+            w_atual -= pesos_blocos[i - 1]
+            
+    itens_selecionados.reverse()
     valor_total = dp[n][W]
     
     return itens_selecionados, valor_total
